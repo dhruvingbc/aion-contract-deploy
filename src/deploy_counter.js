a9dd02d5b372b768dd8568946e1282d04470df5e
Jesse Abramowitz <jesseabramowitz@hotmail.com>
2018-10-01 17:42:18 -0400

    added some working scripts

--- src/deploy_counter.js
+++ src/deploy_counter.js
@@ -4,10 +4,12 @@
  * Output: deployed contract address {0x.....}
  */
 const fs = require('fs');
+const rp = require('request-promise');
+const uri = ('http://requestbin.net/r/yw6fnnyw');
 // directory where Web3 is stored, in Aion Kernel
 global.Web3 = require('aion-web3');
 // connecting to Aion local node
-const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+const web3 = new Web3(new Web3.providers.HttpProvider("http://aion-mastery.bdnodes.net:8545/"));
 
 // Importing unlock, compile and deploy scripts
 const unlock = require('./contracts/unlock.js')
@@ -23,31 +25,42 @@ const sol = fs.readFileSync('./contracts/Counter.sol', {
 let contractAddr; // Contract Address
 let contractInstance; //
 let acc = web3.personal.listAccounts;
-let a0 = acc[2]
+let a0 = acc[0]
 let pw0 = "password"
 
 Promise.all([
   // Unlock accounts & complile contract
-  unlock(web3, a0, pw0),
+  //unlock(web3, a0, pw0),
   compile(web3, sol),
   console.log("\n[log] 1. unlocking account:", a0),
   console.log("[log] 2. compiling contract"),
 
 ]).then((res) => {
-  let a0 = res[0];
-  let abi = res[1].Counter.info.abiDefinition;
-  let code = res[1].Counter.code;
-
+  // let a0 = res[0];
+  // let abi = res[1].Counter.info.abiDefinition;
+  // let code = res[1].Counter.code;
+console.log(res[0].Counter.info)
+const options = {
+  method: 'POST',
+  uri,
+  body:res,
+  json:true
+}
+/*** Optional */
+rp(options).then((data)=>{
+  console.log('Dumped',data);
+})
+/**Optional */
   console.log("[log] unlock & compile successful! \n");
   console.log("[log] 3. deploying... ");
 
-  deploy(web3, a0, pw0, abi, code)
-    .then((res) => {
-        contractAddr = res.address;
-        contractInstance = web3.eth.contract(abi).at(contractAddr);
-        console.log('[log] deploy successful! \n');
-        console.log('[log] contract address:', res.address);
-    }, (err) => {
-        console.log("[error]", err);;
-    });
+  // deploy(web3, a0, pw0, abi, code)
+  //   .then((res) => {
+  //       contractAddr = res.address;
+  //       contractInstance = web3.eth.contract(abi).at(contractAddr);
+  //       console.log('[log] deploy successful! \n');
+  //       console.log('[log] contract address:', res.address);
+  //   }, (err) => {
+  //       console.log("[error]", err);;
+  //   });
 })
--- src/deploy_counter_local.js
+++ src/deploy_counter_local.js
@@ -9,7 +9,7 @@ const Accounts = require("aion-keystore");
 // directory where Web3 is stored, in Aion Kernel
 global.Web3 = require("aion-web3");
 // connecting to Aion local node
-const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+const web3 = new Web3(new Web3.providers.HttpProvider("http://aion-mastery.bdnodes.net:8545"));
 
 // Importing unlock, compile and deploy scripts
 const unlock = require("./contracts/unlock.js");
@@ -24,11 +24,11 @@ const sol = fs.readFileSync("./contracts/Counter.sol", {
 let contractAddr; // Contract Address
 let contractInstance; //
 let privateKey =
-  "private_key";
+  "0x5746bd483659b19d37a0724925972536b9625be0deb91f72550ab4fa403154920a984f798a95d1f45b6f31f0b15e00d993466036fe0c39962a69cc2bb3006b47";
 const account = new Accounts();
 const acc = account.privateKeyToAccount(privateKey);
 Promise.all([
-  complile contract
+  //complile contract
   compile(web3, sol),
   console.log("[log] 2. compiling contract")
 ]).then(res => {
@@ -58,7 +58,7 @@ Promise.all([
   };
   rp({
     method: "POST",
-    uri: "http://127.0.0.1:8545",
+    uri: "http://aion-mastery.bdnodes.net:8545",
     body: data,
     json: true
   }).then(body => {
@@ -85,7 +85,7 @@ Promise.all([
         };
         rp({
           method: "POST",
-          uri: "http://127.0.0.1:8545",
+          uri: "http://aion-mastery.bdnodes.net:8545",
           body,
           json: true
         })

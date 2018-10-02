de869310286417b9cb53389d8dffa5d5c016b7f8
Jesse Abramowitz <jesseabramowitz@hotmail.com>
2018-10-02 15:00:10 -0400

    renamed files

--- src/deploy_ToDo.js
+++ src/deploy_ToDo.js
@@ -9,7 +9,7 @@ const Accounts = require("aion-keystore");
 // directory where Web3 is stored, in Aion Kernel
 global.Web3 = require("aion-web3");
 // connecting to Aion local node
-const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+const web3 = new Web3(new Web3.providers.HttpProvider("http://aion-mastery.bdnodes.net:8545"));
 
 // Importing unlock, compile and deploy scripts
 const unlock = require("./contracts/unlock.js");
@@ -17,24 +17,24 @@ const compile = require("./contracts/compile.js");
 const deploy = require("./contracts/deploy.js");
 const readlineSync = require("readline-sync");
 
-const sol = fs.readFileSync("./contracts/Counter.sol", {
+const sol = fs.readFileSync("./contracts/ToDo.sol", {
   encoding: "utf8"
 });
 
 let contractAddr; // Contract Address
 let contractInstance; //
 let privateKey =
-  "private_key";
+  "0x5746bd483659b19d37a0724925972536b9625be0deb91f72550ab4fa403154920a984f798a95d1f45b6f31f0b15e00d993466036fe0c39962a69cc2bb3006b47";
 const account = new Accounts();
 const acc = account.privateKeyToAccount(privateKey);
 Promise.all([
-  complile contract
+  //complile contract
   compile(web3, sol),
   console.log("[log] 2. compiling contract")
 ]).then(res => {
   let a0 = res[0];
-  let abi = res[0].Counter.info.abiDefinition;
-  let code = res[0].Counter.code;
+  let abi = res[0].ToDo.info.abiDefinition;
+  let code = res[0].ToDo.code;
 
   console.log("[log]compile successful! \n");
   // get NRG estimate for contract
@@ -48,7 +48,7 @@ Promise.all([
   });
   
 
-  let tempNonce = "";
+  let nonce = web3.eth.getTransactionCount(acc.address)
 
   const data = {
     jsonrpc: "2.0",
@@ -58,7 +58,7 @@ Promise.all([
   };
   rp({
     method: "POST",
-    uri: "http://127.0.0.1:8545",
+    uri: "http://aion-mastery.bdnodes.net:8545",
     body: data,
     json: true
   }).then(body => {
@@ -66,8 +66,8 @@ Promise.all([
     console.log("Nonce => ", tempNonce);
     const transaction = {
       nonce: tempNonce,
-      gasPrice: web3.eth.gasPrice,
-      gasLimit: estimate,
+      gasPrice: 100000000000,
+      gas: 2200000,
       data: contractData,
       timestamp: Date.now() * 1000
     };
@@ -85,7 +85,7 @@ Promise.all([
         };
         rp({
           method: "POST",
-          uri: "http://127.0.0.1:8545",
+          uri: "http://aion-mastery.bdnodes.net:8545",
           body,
           json: true
         })
--- src/interact_counter.js
+++ src/interact_counter.js
@@ -6,42 +6,34 @@ const fs = require('fs');
 // directory where Web3 is stored, in Aion Kernel
 global.Web3 = require('aion-web3');
 // connecting to Aion local node
-const web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.1.201:8545"));
+const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 
 // Importing unlock, compile and deploy scripts
 const unlock = require('./contracts/unlock.js')
 const compile = require('./contracts/compile.js');
 const deploy = require('./contracts/deploy.js');
 const readlineSync = require('readline-sync');
-const Accounts = require("aion-keystore");
-
 const sol = fs.readFileSync('./contracts/Counter.sol', {
   encoding: 'utf8'
 });
 
 let contractAddr = process.argv[2]; // Contract Address
 let contractInstance;
-let privateKey =
-  "0x5746bd483659b19d37a0724925972536b9625be0deb91f72550ab4fa403154920a984f798a95d1f45b6f31f0b15e00d993466036fe0c39962a69cc2bb3006b47";
-// let acc = web3.personal.listAccounts;
+let acc = web3.personal.listAccounts;
 let events;
-// let a0 = acc[2];
-// let pw0 = "_password_";
-
-const account = new Accounts();
-
-const acc = account.privateKeyToAccount(privateKey);
+let a0 = acc[2];
+let pw0 = "_password_";
 
 Promise.all([
   // Unlock accounts & complile contract
-  // unlock(web3, a0, pw0),
+  unlock(web3, a0, pw0),
   compile(web3, sol),
-  console.log("\n[log] 1. unlocking account:"),
+  console.log("\n[log] 1. unlocking account:", a0),
   console.log("[log] 2. compiling contract"),
 
 ]).then((res) => {
   let a0 = res[0];
-  let abi = res[0].Counter.info.abiDefinition;
+  let abi = res[1].Counter.info.abiDefinition;
   let code = res[1].Counter.code;
   console.log("[log] accessing contract\n");
   // Contract Instantiantion
@@ -76,57 +68,57 @@ Promise.all([
     }
 
   });
-   readlineSync.promptCLLoop({
+  // readlineSync.promptCLLoop({
 
-     // return count
-     getCount: function () {
-       let count = contractInstance.getCount();
-       console.log("counter =", count.toString(), "\n");
-     },
+  //   // return count
+  //   getCount: function () {
+  //     let count = contractInstance.getCount();
+  //     console.log("counter =", count.toString(), "\n");
+  //   },
 
-     // increment counter by 1
-     incrementCounter: function () {
-       console.log("The incr counter : ", contractInstance.incrementCounter)
-       const hash = contractInstance.incrementCounter(
-         {
-           from: privateKey,
-           data: code
-         }
-       )
+  //   // increment counter by 1
+  //   incrementCounter: function () {
+  //     console.log("The incr counter : ", contractInstance.incrementCounter)
+  //     const hash = contractInstance.incrementCounter(
+  //       {
+  //         from: a0,
+  //         data: code
+  //       }
+  //     )
 
-       console.log("The hash : ", hash);
-       console.log('[log] counter increased \n');
-       events = contractInstance.CounterIncreased({}, { fromBlock: 2627, toBlock: 'latest' });
-       console.log(`event => ${JSON.stringify(events)}`);
-       events.watch(function (err, res) {
-         if (err) {
-           console.log(`err => ${JSON.stringify(err)}`);
-         }
-         else {
-           console.log(`res => ${JSON.stringify(res)}`);
-         }
+  //     console.log("The hash : ", hash);
+  //     console.log('[log] counter increased \n');
+  //     events = contractInstance.CounterIncreased({}, { fromBlock: 2627, toBlock: 'latest' });
+  //     console.log(`event => ${JSON.stringify(events)}`);
+  //     events.watch(function (err, res) {
+  //       if (err) {
+  //         console.log(`err => ${JSON.stringify(err)}`);
+  //       }
+  //       else {
+  //         console.log(`res => ${JSON.stringify(res)}`);
+  //       }
 
-       })
-     },
-     //tx receipt
-     receipt: function (hash) {
-       // get receipt for given tx hash
-       let txReceipt = web3.eth.getTransactionReceipt(hash);
-    
- // print tx receipt
-     console.log("transaction receipt:");
-      console.log(txReceipt);
-   },
-      //decrement counter by 1
-     decrementCounter: function () {
-       contractInstance.decrementCounter(
-         {
-           from: privateKey,
-           data: code,
-           gas: 50000
-         }
-       )
-       console.log('[log] counter decreased\n');
-     }
-   })
+  //     })
+  //   },
+  //   //tx receipt
+  //   receipt: function (hash) {
+  //     // get receipt for given tx hash
+  //     let txReceipt = web3.eth.getTransactionReceipt(hash);
+
+  //     // print tx receipt
+  //     console.log("transaction receipt:");
+  //     console.log(txReceipt);
+  //   },
+  //   // decrement counter by 1
+  //   decrementCounter: function () {
+  //     contractInstance.decrementCounter(
+  //       {
+  //         from: a0,
+  //         data: code,
+  //         gas: 50000
+  //       }
+  //     )
+  //     console.log('[log] counter decreased\n');
+  //   }
+  // })
 });
